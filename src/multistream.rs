a59f577bb74b41deba8ef1ddb359dc55ef53e9c4
Wim Looman <wim@nemo157.com>
2016-07-10 13:00:02 +0200

    Implement read/write for multistream

--- src/multistream.rs
+++ src/multistream.rs
@@ -37,3 +37,35 @@ impl<S> MultiStream<S> where S: io::Read + io::Write {
         }
     }
 }
+
+impl<S> io::Read for MultiStream<S> where S: io::Read + io::Write {
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        self.stream.read(buf)
+    }
+
+    fn read_to_end(&mut self, buf: &mut Vec<u8>) -> io::Result<usize> {
+        self.stream.read_to_end(buf)
+    }
+
+    fn read_to_string(&mut self, buf: &mut String) -> io::Result<usize> {
+        self.stream.read_to_string(buf)
+    }
+
+    fn read_exact(&mut self, buf: &mut [u8]) -> io::Result<()> {
+        self.stream.read_exact(buf)
+    }
+}
+
+impl<S> io::Write for MultiStream<S> where S: io::Read + io::Write {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        self.stream.write(buf)
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        self.stream.flush()
+    }
+
+    fn write_all(&mut self, buf: &[u8]) -> io::Result<()> {
+        self.stream.write_all(buf)
+    }
+}
